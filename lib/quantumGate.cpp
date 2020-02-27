890bb28066f9ab12f579f6a01e523021f8e7b1a7
KrolSeb <Sebastian.Krol@Live.umcs.edu.pl>
2020-02-27 10:41:23 +0100

    Renamed CNOT to NOT gate.

--- lib/quantumGate.cpp
+++ lib/quantumGate.cpp
@@ -2,15 +2,15 @@
 #include "headers/quantumGate.h"
 
 /// @params - matrix representations of quantum logic gates
-const int CNOT_MATRIX_DIMENSION = 2;
-const int CNOT_LOGIC_GATE[][CNOT_MATRIX_DIMENSION] = {{0, 1},
-                                                      {1, 0}};
+const int NOT_GATE_DIMENSION = 2;
+const int NOT_QUANTUM_GATE[][NOT_GATE_DIMENSION] = {{0, 1},
+                                                    {1, 0}};
 
-/// Function used to show matrix on output - int type
+/// Function used to show quantum gate on output - int type
 void printLogicGateRepresentation() {
-    for (int i = 0; i < CNOT_MATRIX_DIMENSION; i++) {
-        for (int j = 0; j < CNOT_MATRIX_DIMENSION; j++) {
-            cout << CNOT_LOGIC_GATE[i][j];
+    for (int i = 0; i < NOT_GATE_DIMENSION; i++) {
+        for (int j = 0; j < NOT_GATE_DIMENSION; j++) {
+            cout << NOT_QUANTUM_GATE[i][j];
         }
         cout << endl;
     }
