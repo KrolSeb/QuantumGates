aaefe3b4beee5a836888e97c26c6efcfc4c77df3
KrolSeb <Sebastian.Krol@Live.umcs.edu.pl>
2020-03-01 07:30:02 +0100

    Implemented negation operation for previous defined NOT quantum gate.

--- lib/headers/quantumGate.h
+++ lib/headers/quantumGate.h
@@ -1,7 +1,10 @@
 #ifndef QUANTUMGATES_QUANTUMGATE_H
 #define QUANTUMGATES_QUANTUMGATE_H
-using namespace std;
 
-void printLogicGateRepresentation();
+/// @params - constans sizes of quantum gates
+const int NOT_GATE_ROWS = 2, NOT_GATE_COLUMNS = 2;
+
+int ** getNotGate();
+int ** makeNotOnQubit(int **qubit, const int QUBIT_COLUMNS);
 
 #endif //QUANTUMGATES_QUANTUMGATE_H
--- lib/quantumGate.cpp
+++ lib/quantumGate.cpp
@@ -1,18 +1,64 @@
 #include <iostream>
 #include "headers/quantumGate.h"
+#include "headers/temporaryQubit.h"
+
+using namespace std;
 
 /// @params - matrix representations of quantum logic gates
-const int NOT_GATE_DIMENSION = 2;
-const int NOT_QUANTUM_GATE[][NOT_GATE_DIMENSION] = {{0, 1},
-                                                    {1, 0}};
+const int NOT_QUANTUM_GATE[][NOT_GATE_COLUMNS] = {{0, 1},
+                                                  {1, 0}};
+
+//TODO: Implementation of all quantum gates
+//TODO: Reverse and combining/folding of quantum gates
 
 /// Function used to show quantum gate on output - int type
-void printLogicGateRepresentation() {
-    for (int i = 0; i < NOT_GATE_DIMENSION; i++) {
-        for (int j = 0; j < NOT_GATE_DIMENSION; j++) {
+void printNotGateRepresentation() {
+    for (int i = 0; i < NOT_GATE_ROWS; i++) {
+        for (int j = 0; j < NOT_GATE_COLUMNS; j++) {
             cout << NOT_QUANTUM_GATE[i][j];
         }
         cout << endl;
     }
     cout << endl;
+}
+
+void setNotGateValues(int **notGateRepresentation) {
+    for (int i = 0; i < NOT_GATE_ROWS; i++) {
+        for (int j = 0; j < NOT_GATE_COLUMNS; j++) {
+            notGateRepresentation[i][j] = NOT_QUANTUM_GATE[i][j];
+        }
+    }
+}
+
+int **getNotGate() {
+    int **notGateRepresentation = new int *[NOT_GATE_ROWS];
+    for (int i = 0; i < NOT_GATE_ROWS; i++)
+        notGateRepresentation[i] = new int[NOT_GATE_COLUMNS];
+
+    setNotGateValues(notGateRepresentation);
+    printNotGateRepresentation();
+    return notGateRepresentation;
+}
+
+//Function used to make NOT(negation) on qubit
+int **makeNotOnQubit(int **qubit, const int QUBIT_COLUMNS) {
+    int sum;
+    int **outputQubit = new int * [NOT_GATE_ROWS];
+    for (int i = 0; i < NOT_GATE_ROWS; i++) {
+        outputQubit[i] = new int [QUBIT_COLUMNS];
+    }
+
+    //Matrix multiplication
+    for (int i = 0; i < NOT_GATE_ROWS; i++) {
+        for (int j = 0; j < QUBIT_COLUMNS; j++) {
+            sum = 0;
+            for (int k = 0; k < NOT_GATE_COLUMNS; k++) {
+                sum += NOT_QUANTUM_GATE[i][k] * qubit[k][j];
+            }
+            outputQubit[i][j] = sum;
+        }
+    }
+
+    printSingleQubit(outputQubit);
+    return outputQubit;
 }
