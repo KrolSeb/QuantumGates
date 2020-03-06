30e8f5176e7a79f3d092f74f86148d596ebfb824
KrolSeb <Sebastian.Krol@Live.umcs.edu.pl>
2020-03-06 10:18:06 +0100

    Added new constant parameters. Added function definitions for quantum gates operations.

--- lib/headers/quantumGate.h
+++ lib/headers/quantumGate.h
@@ -2,14 +2,19 @@
 #define QUANTUMGATES_QUANTUMGATE_H
 #include <complex>
 
-/// @params - constans sizes of quantum gates
-const int NOT_GATE_ROWS = 2, NOT_GATE_COLUMNS = 2;
+/// @params - constant sizes of quantum gates representations
+const int ONE_ARGUMENT_GATE_SIZE = 2;
+const int TWO_ARGUMENTS_GATE_SIZE = 4;
+const int THREE_ARGUMENTS_GATE_SIZE = 8;
 
-/// @functions
-int **getNotGate();
-int **makeNotOnQubit(int **qubit, const int QUBIT_COLUMNS);
+double *makeNotOnQubit(double *qubit, const int QUBIT_COLUMNS);
+double *makeCnotOnQubit(double *qubit, const int QUBIT_COLUMNS);
+double *makeSwapOnQubit(double *qubit, const int QUBIT_COLUMNS);
+double *makeFredkinOnQubit(double *qubit, const int QUBIT_COLUMNS);
+double *makeToffoliOnQubit(double *qubit, const int QUBIT_COLUMNS);
+
+double **getNotGate();
 std::complex<double> **multiplySqrtNotScalarByMatrix();
 std::complex<double> **multiplyHadamardScalarByMatrix();
-
 
 #endif //QUANTUMGATES_QUANTUMGATE_H
