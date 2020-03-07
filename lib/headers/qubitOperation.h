bbcd4583eb210361b7177c54999b5d776ab32889
KrolSeb <Sebastian.Krol@Live.umcs.edu.pl>
2020-03-07 10:37:52 +0100

    Implemented printSingleComplexQubit function.

--- lib/headers/qubitOperation.h
+++ lib/headers/qubitOperation.h
@@ -2,6 +2,7 @@
 #define QUANTUMGATES_QUBITOPERATION_H
 #include <algorithm>
 #include <vector>
+#include <complex>
 
 const int SINGLE_QUBIT_ROWS = 2;
 const int TWO_QUBIT_ROWS = 4;
@@ -9,6 +10,7 @@ const int THREE_QUBIT_ROWS = 8;
 const int QUBIT_COLUMNS = 1;
 
 void printSingleQubit(double *singleQubit);
+void printSingleComplexQubit(std::complex<double> *singleQubit);
 void printTwoQubits(double *qubits);
 void printThreeQubits(double *qubits);
 double *getBaseVectorAsArray(std::vector<double> baseVector);
--- lib/qubitOperation.cpp
+++ lib/qubitOperation.cpp
@@ -10,6 +10,21 @@ void printSingleQubit(double *singleQubit) {
     cout << endl;
 }
 
+void printSingleComplexQubit(complex<double> *singleQubit) {
+    for (int i = 0; i < SINGLE_QUBIT_ROWS; i++) {
+        if (imag(singleQubit[i]) >= 0) {
+            cout << real(singleQubit[i]) << "+" << imag(singleQubit[i]) << "i" << endl;
+        }
+        else if (real(singleQubit[i]) == 0 && imag(singleQubit[i]) == 0) {
+            cout << real(singleQubit[i]) << endl;
+        }
+        else {
+            cout << real(singleQubit[i]) << imag(singleQubit[i]) << "i" << endl;
+        }
+    }
+    cout << endl;
+}
+
 void printTwoQubits(double *qubits) {
     for (int i = 0; i < TWO_QUBIT_ROWS; i++) {
         cout << qubits[i] << endl;
