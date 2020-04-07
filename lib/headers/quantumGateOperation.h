43b932898d7efa1787b749715642ab8cbcb8f646
KrolSeb <Sebastian.Krol@Live.umcs.edu.pl>
2020-04-07 10:32:47 +0200

    Deleted unnecessary numericMatrix.cpp and header files. Created quantumGateOperation.cpp and header files. Implemented procedures used to check if identity matrix and multiply of quantum gates are equal. Updated CMakeLists.txt.

---
+++ lib/headers/quantumGateOperation.h
@@ -0,0 +1,7 @@
+#ifndef QUANTUMGATES_QUANTUMGATEOPERATION_H
+#define QUANTUMGATES_QUANTUMGATEOPERATION_H
+#include <complex>
+
+bool isAssemblyOfGatesGiveIdentityMatrix(double **firstGate, double **secondGate, int gateSize);
+
+#endif //QUANTUMGATES_QUANTUMGATEOPERATION_H
---
+++ lib/quantumGateOperation.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+using namespace std;
+
+double **assembleQuantumGates(double **firstGate, double **secondGate, int gateSize) {
+    double **outputGate = new double *[gateSize];
+    for (int i = 0; i < gateSize; i++) {
+        outputGate[i] = new double[gateSize];
+    }
+
+    for (int i = 0; i < gateSize; i++) {
+        for (int j = 0; j < gateSize; j++) {
+            for (int k = 0; k < gateSize; k++) {
+                outputGate[i][j] += firstGate[i][k] * secondGate[k][j];
+            }
+        }
+    }
+
+    return outputGate;
+}
+
+double **getIdentityMatrixForDefinedSize(int gateSize) {
+    double **identityMatrix = new double *[gateSize];
+    for (int i = 0; i < gateSize; i++) {
+        identityMatrix[i] = new double[gateSize];
+    }
+
+    for (int i = 0; i < gateSize; i++) {
+        for (int j = 0; j < gateSize; j++) {
+            if (i == j) {
+                identityMatrix[i][j] = 1;
+            }
+            else {
+                identityMatrix[i][j] = 0;
+            }
+        }
+    }
+
+    return identityMatrix;
+}
+
+bool isIdentityMatrixAndMultipliedGatesAreEqual(double **identityMatrix, double **outputGate, int gateSize) {
+    for (int i = 0; i < gateSize; i++) {
+        for (int j = 0; j < gateSize; j++) {
+            if (identityMatrix[i][j] != outputGate[i][j]) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+bool isAssemblyOfGatesGiveIdentityMatrix(double **firstGate, double **secondGate, int gateSize) {
+    double **identityMatrix = getIdentityMatrixForDefinedSize(gateSize);
+    double **outputGate = assembleQuantumGates(firstGate, secondGate, gateSize);
+
+    return isIdentityMatrixAndMultipliedGatesAreEqual(identityMatrix, outputGate, gateSize);
+}
+
+
