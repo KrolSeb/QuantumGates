0e914838a7f8f3a2cfdc1a51e4fa855be0acaebf
KrolSeb <Sebastian.Krol@Live.umcs.edu.pl>
2020-04-18 10:09:04 +0200

    Added test used to check that normalization of base vector works properly for incorrect probabilities.

--- quantum-tests/tests/tests.cpp
+++ quantum-tests/tests/tests.cpp
@@ -6,9 +6,10 @@
 #include "headers/qubitOperation.h"
 
 using namespace std;
+using namespace quantum;
 
-/// Test to check if matrices are equal (original and conjugate transposed one)
-TEST(hermitianMatrix, equalsOfMatricesElements) {
+/// Test to check that matrices are equal (original and conjugate transposed one)
+TEST(matrixOperation, equalsOfHermitianMatrices) {
     srand(static_cast<unsigned int>(time(nullptr)));
     int dimension = rand() % 4 + 2;
     complex<double> **hermitianMatrix = getRandomHermitianMatrix(dimension);
@@ -19,4 +20,27 @@ TEST(hermitianMatrix, equalsOfMatricesElements) {
             ASSERT_EQ(hermitianMatrix[i][j], hermitianMatrixAfterConjugateTranspose[i][j]);
         }
     }
+}
+
+/// Test to check that normalization of base vector works properly for incorrect probabilities
+TEST(quantumComputer, isSumOfProbabilitiesAfterNormalizationEqualOne) {
+    int numberOfQubits = 3;
+    int arraySize = pow(2, numberOfQubits);
+    double probabilities[] = {4.0, 0.0, 3.0, 0.0, 4.0, 0.0, 3.0, 0.0};
+
+    /// Definition of structure
+    /// Method validateProbability() is launched and when (sum of probabilities are != 1)
+    /// normalizeRegister and resetState methods are executed (correction of probabilities)
+    struct QuantumComputer quantumComputer = QuantumComputer(numberOfQubits, probabilities, arraySize);
+
+    /// Show probabilities after normalization
+    quantumComputer.viewProbability();
+
+    /// Sum of probabilities after correction (it should be 1)
+    double result = 0.0;
+    for (auto x: quantumComputer.baseVector) {
+        result += pow(sqrt(fabs(x)), 2);
+    }
+
+    ASSERT_EQ(1.0, result);
 }
