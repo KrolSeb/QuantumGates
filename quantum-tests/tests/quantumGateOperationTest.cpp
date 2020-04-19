dcb06b8aeff5cd344a68fa183fb269b4a343f9cb
KrolSeb <Sebastian.Krol@Live.umcs.edu.pl>
2020-04-19 22:37:40 +0200

    Implemented tests for checking unitary of matrices and properly composing quantum gates.

--- quantum-tests/tests/quantumGateOperationTest.cpp
+++ quantum-tests/tests/quantumGateOperationTest.cpp
@@ -1,10 +1,28 @@
 #include "gtest/gtest.h"
-#include <headers/quantumGate.h>
+#include "headers/quantumGate.h"
 #include "headers/quantumGateOperation.h"
+#include "headers/matrixOperation.h"
 #include <iostream>
 #include <complex>
 
 using namespace std;
+
+/// Helper method used to get not unitary matrix (determinant not equal 1)
+complex<double> **getNotUnitaryMatrix(int matrixSize) {
+    complex<double> **matrixWithZeroDeterminant = new complex<double>*[matrixSize];
+    for (int i = 0; i < matrixSize; i++) {
+        matrixWithZeroDeterminant[i] = new complex<double>[matrixSize];
+    }
+
+    for (int i = 0; i < matrixSize; i++) {
+        for (int j = 0; j < matrixSize; j++) {
+            matrixWithZeroDeterminant[i][j] = complex<double>(0,0);
+        }
+    }
+
+    return matrixWithZeroDeterminant;
+}
+
 
 TEST(quantumGateOperation, testGenerationOfHadamardGateForIndex0) {
     int baseNumber = 2;
@@ -109,5 +127,57 @@ TEST(quantumGateOperation, testGenerationOfPhaseShiftGateForPiDividedByTwo) {
             ASSERT_EQ(expectedQubit[i][j], phaseShiftGate[i][j]);
         }
     }
+}
 
+TEST(quantumGateOperation, testComposeOfTwoNotQuantumGates) {
+    ASSERT_TRUE(isComposeOfGatesGivesIdentityMatrix(getNotGate(), getNotGate(), ONE_ARGUMENT_GATE_SIZE));
+}
+
+TEST(quantumGateOperation, testComposeOfTwoHadamardQuantumGates) {
+    ASSERT_TRUE(isComposeOfGatesGivesIdentityMatrix(getHadamardGate(), getHadamardGate(), ONE_ARGUMENT_GATE_SIZE));
+}
+
+TEST(quantumGateOperation, testComposeOfTwoSwapQuantumGates) {
+    ASSERT_TRUE(isComposeOfGatesGivesIdentityMatrix(getSwapGate(), getSwapGate(), TWO_ARGUMENTS_GATE_SIZE));
+}
+
+TEST(quantumGateOperation, testComposeOfTwoPhaseShiftQuantumGatesForPI) {
+    ASSERT_TRUE(isComposeOfGatesGivesIdentityMatrix(getPhaseShiftGate(M_PI), getPhaseShiftGate(M_PI), ONE_ARGUMENT_GATE_SIZE));
+}
+
+TEST(quantumGateOperation, testComposeOfTwoPhaseShiftQuantumGatesForPiAndMinusPi) {
+    ASSERT_TRUE(isComposeOfGatesGivesIdentityMatrix(getPhaseShiftGate(M_PI), getPhaseShiftGate(-M_PI), ONE_ARGUMENT_GATE_SIZE));
+}
+
+TEST(quantumGateOperation, testComposeOfTwoPhaseShiftQuantumGatesForPiAndPiDividedByTwo) {
+    ASSERT_FALSE(isComposeOfGatesGivesIdentityMatrix(getPhaseShiftGate(M_PI), getPhaseShiftGate(M_PI/2), ONE_ARGUMENT_GATE_SIZE));
+}
+
+TEST(quantumGateOperation, testComposeOfCnotAndSwapQuantumGates) {
+    ASSERT_FALSE(isComposeOfGatesGivesIdentityMatrix(getCnotGate(), getSwapGate(), TWO_ARGUMENTS_GATE_SIZE));
+}
+
+TEST(quantumGateOperation, testIsCnotGatesUnitary) {
+    complex<double> **cnotGate = getCnotGate();
+    complex<double> **conjugateTransposedCnotGate = makeConjugateTranspose(getCnotGate(), TWO_ARGUMENTS_GATE_SIZE, TWO_ARGUMENTS_GATE_SIZE);
+
+    ASSERT_TRUE(isMatrixUnitary(cnotGate, conjugateTransposedCnotGate, TWO_ARGUMENTS_GATE_SIZE));
+}
+
+TEST(quantumGateOperation, testNotGateAndZeroDeterminantMatrixNotUnitary) {
+    complex<double> **notGate = getNotGate();
+    complex<double> **conjugateTransposedMatrix = makeConjugateTranspose(
+            getNotUnitaryMatrix(ONE_ARGUMENT_GATE_SIZE),
+            ONE_ARGUMENT_GATE_SIZE, ONE_ARGUMENT_GATE_SIZE);
+
+    ASSERT_FALSE(isMatrixUnitary(notGate, conjugateTransposedMatrix, ONE_ARGUMENT_GATE_SIZE));
+}
+
+TEST(quantumGateOperation, testTwoZeroDeterminantMatricesNotUnitary) {
+    complex<double> **notUnitaryMatrix = getNotUnitaryMatrix(ONE_ARGUMENT_GATE_SIZE);
+    complex<double> **conjugateTransposedMatrix = makeConjugateTranspose(
+            getNotUnitaryMatrix(ONE_ARGUMENT_GATE_SIZE),
+            ONE_ARGUMENT_GATE_SIZE, ONE_ARGUMENT_GATE_SIZE);
+
+    ASSERT_FALSE(isMatrixUnitary(notUnitaryMatrix, conjugateTransposedMatrix, ONE_ARGUMENT_GATE_SIZE));
 }
